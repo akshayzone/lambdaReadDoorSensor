45bd0ea936813555e42c4dfb3524ff61ff8a19ef
Perinei <adilson.perinei@yahoo.com>
2019-04-11 10:32:54 -0400

    ready

---
+++ index.js
@@ -0,0 +1,19 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: 'us-east-1' });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function(event, context, callback) {
+    docClient.scan({
+        TableName: 'door_sensor'
+    }, (err, data)=>{
+        if(err) {
+            // console.log(err);
+            callback(err, null);
+        } else {
+            // console.log(data);     
+            callback(null, data);
+        }
+        
+    });
+}
